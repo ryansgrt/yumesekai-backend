e1b04f88d2b8674b152ee28625ead19916980e10
unknown <ryansgrt23@gmail.com>
2021-02-20 09:24:59 +0700

    Initial Commit

---
+++ src/main/java/com/snflwrsdev/yumesekai/YumesekaiApplication.java
@@ -0,0 +1,13 @@
+package com.snflwrsdev.yumesekai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumesekaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumesekaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snflwrsdev/yumesekai/YumesekaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snflwrsdev.yumesekai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumesekaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
